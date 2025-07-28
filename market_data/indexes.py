0b37be60e1260e40bcbd3a0fd43420d13845727c
syple000 <1519761174@qq.com>
2025-07-28 21:32:29 +0800

    打印指数symbols

--- market_data/indexes.py
+++ market_data/indexes.py
@@ -3,14 +3,20 @@
 from .models import Symbol, MarketType, Type
 
 INDEXES = [
-    Symbol(code='000001', name='上证指数', market=MarketType.SH.value, type=Type.INDEX.value),
-    Symbol(code='399001', name='深证成指', market=MarketType.SZ.value, type=Type.INDEX.value),
-    Symbol(code='399006', name='创业板指', market=MarketType.SZ.value, type=Type.INDEX.value),
-    Symbol(code='000300', name='沪深300', market=MarketType.SH.value, type=Type.INDEX.value),
-    Symbol(code='000905', name='中证500', market= MarketType.SH.value, type=Type.INDEX.value),
-    Symbol(code='000016', name='上证50', market=MarketType.SH.value, type=Type.INDEX.value),
-    Symbol(code='000852', name='中证1000', market=MarketType.SH.value, type=Type.INDEX.value),
-    Symbol(code='399005', name='中小板指', market=MarketType.SZ.value, type=Type.INDEX.value),
-    Symbol(code='000688', name='科创50', market=MarketType.SH.value, type=Type.INDEX.value),
-    Symbol(code='000903', name='中证100', market= MarketType.SH.value, type=Type.INDEX.value),
+    Symbol(code='000001', market=MarketType.SH.value, type=Type.INDEX.value), # 上证指数
+    Symbol(code='399001', market=MarketType.SZ.value, type=Type.INDEX.value), # 深证成指
+    Symbol(code='399006', market=MarketType.SZ.value, type=Type.INDEX.value), # 创业板指
+    Symbol(code='000300', market=MarketType.SH.value, type=Type.INDEX.value), # 沪深300
+    Symbol(code='000905', market=MarketType.SH.value, type=Type.INDEX.value), # 中证500
+    Symbol(code='000016', market=MarketType.SH.value, type=Type.INDEX.value), # 上证50
+    Symbol(code='000852', market=MarketType.SH.value, type=Type.INDEX.value), # 中证1000
+    Symbol(code='399005', market=MarketType.SZ.value, type=Type.INDEX.value), # 中小板指
+    Symbol(code='000688', market=MarketType.SH.value, type=Type.INDEX.value), # 科创50
+    Symbol(code='000903', market=MarketType.SH.value, type=Type.INDEX.value), # 中证100
 ]
+
+if __name__ == '__main__':
+    symbols = []
+    for index in INDEXES:
+        symbols.append(index.code + '.' + index.market)
+    print(','.join(symbols))
